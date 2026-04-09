640d713c6581eb7de0edd0bbcdf1737cd906550d
Saru <sarusman.satkunarajah1@gmail.com>
2026-04-09 10:46:24 +0200

    Create Logement Microservice (#7)

--- housing-service/app/__init__.py
+++ housing-service/app/__init__.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
-from .routes.house import houses_bp
+db = SQLAlchemy()
+
 
 def create_app():
     app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///logements.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    app.register_blueprint(houses_bp)
+    db.init_app(app)
+
+    from app.controllers.logement_controller import logement_bp
+    app.register_blueprint(logement_bp)
+
+    with app.app_context():
+        db.create_all()
 
     return app
