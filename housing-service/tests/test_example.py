03de118c99092746926ce9b37cb9325f460aa9de
Sarusman <sarusmandu92@gmail.com>
2026-04-09 10:41:36 +0200

    Create Logement Microservice

--- housing-service/tests/test_example.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-
-def test_example():
-    app = create_app()
-    houses = app.test_client()
-
-    response = houses.get("/houses")
-
-    assert response.status_code == 200
